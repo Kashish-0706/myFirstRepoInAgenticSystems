63d7bbfb0676eac170ad3a5ee0eb8168abcad449
Kashish Faujdar <kashish.kareeli@gmail.com>
2026-02-12 13:09:28 +0530

    Add script to print numbers from 1 to the number that is provided

--- myFirstRepoInAgenticSystems/python-loops-assignment/number-printer.py
+++ myFirstRepoInAgenticSystems/python-loops-assignment/number-printer.py
@@ -1,4 +1,4 @@
-# defining the code which prints number from 0 to the number that is provided.
+# defining  code which prints number from 0 to the number that is provided.
 number = int(input("Enter the number:" ))
 for i in range(0,number+1,1):
     print(i)
