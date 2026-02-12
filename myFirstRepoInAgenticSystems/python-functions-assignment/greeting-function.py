26b830967ad060958856c0df81c69ed4f35c2c3a
Kashish Faujdar <kashish.kareeli@gmail.com>
2026-02-12 18:44:09 +0530

    Implement greeting message script using function

---
+++ myFirstRepoInAgenticSystems/python-functions-assignment/greeting-function.py
@@ -0,0 +1,8 @@
+# defining a function that takes users name as input and returns greeting message
+
+def greet_user(name):
+    return f"Hello, {name}!"
+
+# function call
+message = greet_user("Sanidhya")
+print(message)
