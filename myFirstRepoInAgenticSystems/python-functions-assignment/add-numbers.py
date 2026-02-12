6194c0fd7bf34726de2f352f379a780461faa2d9
Kashish Faujdar <kashish.kareeli@gmail.com>
2026-02-12 19:00:32 +0530

    Add script of adding two numbers using function

---
+++ myFirstRepoInAgenticSystems/python-functions-assignment/add-numbers.py
@@ -0,0 +1,8 @@
+# defining a function that accept two numbers and returns their sum
+
+def add_numbers(number1,number2):
+    return number1 + number2
+
+#function call
+sum = add_numbers(8,7)
+print(f"Sum is: {sum}")
